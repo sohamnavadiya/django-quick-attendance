355a81c8fb936e448fc59183bf744e05f97ef5cd
sohamnavadiya <soham@auberginesolutions.com>
2017-09-15 07:40:26 +0530

    UPDATE

---
+++ quickattendance/README.md
@@ -0,0 +1,3 @@
+# quickattendance
+
+[![Join the chat at https://gitter.im/django-quick-attendance/Lobby](https://badges.gitter.im/django-quick-attendance/Lobby.svg)](https://gitter.im/django-quick-attendance/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
---
+++ quickattendance/quickattendance/attendance/serializers.py
@@ -0,0 +1,55 @@
+from attendance.models import SabhaSession, SabhaType, Attendance
+
+from authentication.serializers import LoginSerializer
+
+from authentication.models import User
+
+from profiles.models import Profile
+
+from authentication.serializers import UserSerializer
+
+from authentication.serializers import DynamicFieldsModelSerializer
+from rest_framework import serializers
+
+
+class SabhaTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SabhaType
+        fields = ('id', 'sabha_type')
+
+
+class PostSabhaSessionSerializer(DynamicFieldsModelSerializer):
+    class Meta:
+        model = SabhaSession
+        fields = ('id', 'user', 'sabhatype', 'date', 'status')
+
+
+class GetSabhaSessionSerializer(DynamicFieldsModelSerializer):
+    sabhatype = SabhaTypeSerializer(read_only=True)
+
+    class Meta:
+        model = SabhaSession
+        fields = ('id', 'user', 'sabhatype', 'date', 'status')
+        depth = 1
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    # session_id = SabhaSessionSerializer(read_only=True)
+    user = UserSerializer(read_only=True, fields=('username',))
+    last_modified_by = UserSerializer(read_only=True, fields=('username',))
+
+    # To change the key name in response
+    session_info = GetSabhaSessionSerializer(read_only=True, source='session_id', fields=('id', 'sabhatype'))
+
+    class Meta:
+        model = Attendance
+        fields = ('status', 'user', 'last_modified_by', 'session_info', 'leave_reason')
+        # To add foreign key value info instead just primary key
+        # depth = 1
+
+
+class AttendanceInsertSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = ('status', 'user', 'last_modified_by', 'session_id', 'leave_reason')
+        # To add foreign key value info instead just primary key
+        # depth = 1
